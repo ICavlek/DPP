fbeebda1dcf9d7a63fc07083b92ee5b643170b34
Ivan <ivan.cavlek1403@gmail.com>
2022-08-20 18:00:31 +0200

    Additional for Demo - Test SUCCESS

--- DataPostProcess/main.py
+++ DataPostProcess/main.py
@@ -2,9 +2,9 @@ from src import update_excel_files, CsvHandlerBaseError
 
 
 if __name__ == '__main__':
-    file_name = "Trade_Report_GBPUSD.csv"
+    file_name = "D:\\Demo\\TradeReport\\Trade_Report_GBPUSD.csv"
     format_file = False
-    target_folder = "ExcelReports"
+    target_folder = "D:\\Demo\\ExcelReports"
     try:
         update_excel_files(file_name, format_file, target_folder)
     except CsvHandlerBaseError as e:
